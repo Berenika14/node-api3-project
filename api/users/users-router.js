2fcf5d0727be818a97f5443ae4a3a42cf9816331
Berenika Ahmetaj <ahmetajberenika@gmail.com>
2022-02-10 01:44:56 -0500

    Succsessfully passed the test to post a new user

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -32,10 +32,14 @@ router.get("/:id", validateUserId, (req, res, next) => {
     .catch(next);
 });
 
-router.post("/", validateUser, (req, res) => {
+router.post("/", validateUser, (req, res, next) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
-  console.log(req.name);
+  User.insert({ name: req.name })
+    .then((user) => {
+      res.status(201).json(user);
+    })
+    .catch(next);
 });
 
 router.put("/:id", validateUserId, validateUser, (req, res) => {
