f3bb240f482fdee742613c1e1d63038351ca8946
Berenika Ahmetaj <ahmetajberenika@gmail.com>
2022-02-10 01:13:09 -0500

    Built validatePost middleware

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -39,9 +39,10 @@ function validateUser(req, res, next) {
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
   const { text } = req.body;
-  if (!text) {
+  if (!text || !text.trim()) {
     res.status(400).json({ message: "missing required text field" });
   } else {
+    req.text = text.trim();
     next();
   }
 }
