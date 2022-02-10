f78a428e8698eca7a00354aa23641d19442f43e4
Berenika Ahmetaj <ahmetajberenika@gmail.com>
2022-02-10 00:18:48 -0500

    built the logger middleware

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,31 @@
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  const timeStamp = new Date().toLocaleDateString();
+  const method = req.method;
+  const url = req.originalUrl;
+  console.log(`${timeStamp}, ${method}, ${url}`);
+  next();
 }
 
 function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  console.log("validateUserId middleware");
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  console.log("validateUser middleware");
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  console.log("validatePost middleware");
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost,
+};
