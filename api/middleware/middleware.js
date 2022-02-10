e238b2c976bc546604f7934dd74ca03eeab6bba4
Berenika Ahmetaj <ahmetajberenika@gmail.com>
2022-02-10 01:01:56 -0500

    Built validateUserID and validateUser middleware

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,3 +1,5 @@
+const User = require("../users/users-model");
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
   const timeStamp = new Date().toLocaleDateString();
@@ -7,19 +9,41 @@ function logger(req, res, next) {
   next();
 }
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
   // DO YOUR MAGIC
-  console.log("validateUserId middleware");
+  try {
+    const user = await User.getById(req.params.id);
+    if (!user) {
+      res.status(404).json({ message: " User doesn't exist" });
+    } else {
+      req.user = user;
+      next();
+    }
+  } catch (err) {
+    //later
+    res.status(500).json({ message: " Error finding the user" });
+  }
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
-  console.log("validateUser middleware");
+  const { name } = req.body;
+  if (!name || !name.trim()) {
+    res.status(400).json({ message: "missing required name field" });
+  } else {
+    req.name = name.trim();
+    next();
+  }
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
-  console.log("validatePost middleware");
+  const { text } = req.body;
+  if (!text) {
+    res.status(400).json({ message: "missing required text field" });
+  } else {
+    next();
+  }
 }
 
 // do not forget to expose these functions to other modules
--- api/server.js
+++ api/server.js
@@ -8,6 +8,8 @@ server.use(logger);
 server.use(express.json());
 
 // global middlewares and the user's router need to be connected here
+const usersRouter = require("./users/users-router");
+server.use("/api/users", usersRouter);
 
 server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
